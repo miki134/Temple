930927ca8c60fd3ff2e65f15f3b0af00feacd761
miki134 <miki.n.2001@gmail.com>
2023-03-10 09:31:54 +0100

    lab2 - two toruses rotate

--- main_file.cpp
+++ main_file.cpp
@@ -19,6 +19,8 @@ Place, Fifth Floor, Boston, MA  02110 - 1301  USA
 
 #define GLM_FORCE_RADIANS
 
+float speed = 0; //Prędkość kątowa obrotu obiektu
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
@@ -37,12 +39,36 @@ void error_callback(int error, const char* description) {
 	fputs(description, stderr);
 }
 
+void key_callback(GLFWwindow* window, int key,
+    int scancode, int action, int mods) {
+    if (action == GLFW_PRESS) {
+        if (key == GLFW_KEY_A) printf("A \n");
+        if (key == GLFW_KEY_D) printf("D \n");
+        if (key == GLFW_KEY_LEFT) {
+            speed -= PI;
+            printf("LEFT \n");
+        }
+        if (key == GLFW_KEY_RIGHT) {
+            speed += PI;
+            printf("RIGHT \n");
+        }
+        if (key == GLFW_KEY_W && (mods & GLFW_MOD_ALT) != 0)
+            printf("ALT+W\n");
+    }
+    if (action == GLFW_RELEASE) {
+        if (key == GLFW_KEY_W) printf("puszczone W\n");
+
+        speed = 0; //Jeżeli puszczono klawisz ustaw prędkość obrotu na zero
+    }
+}
+
 //Procedura inicjująca
 void initOpenGLProgram(GLFWwindow* window) {
     initShaders();
-	//************Tutaj umieszczaj kod, który należy wykonać raz, na początku programu************
-    glEnable(GL_DEPTH_TEST);
-    glClearColor(0, 0, 0, 0);
+    //************Tutaj umieszczaj kod, który należy wykonać raz, na początku programu************
+    glClearColor(0, 0, 0, 1);//Ustaw czarny kolor czyszczenia ekranu
+    glEnable(GL_DEPTH_TEST); //Włącz test głębokości pikseli
+    glfwSetKeyCallback(window, key_callback); //Zarejestruj procedurę obsługi klawiatury
 }
 
 
@@ -57,36 +83,46 @@ void drawScene(GLFWwindow* window, float angle) {
 	//************Tutaj umieszczaj kod rysujący obraz******************l
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+    spLambert->use();
 
     glm::mat4 M = glm::mat4(1.0f);
-    M =glm::rotate(M,angle,glm::vec3(0.0f,1.0f,0.0f));
+    glm::mat4 M1;
+    glm::mat4 M2;
+
+    M1 = glm::rotate(M, angle, glm::vec3(1.0f, 0.0f, 0.0f));
+    M2 = glm::rotate(M, -angle, glm::vec3(1.0f, 0.0f, 0.0f));
+
+    M1 = glm::translate(M1, glm::vec3(1.0f, 0.0f, 0.0f));
+    M2 = glm::translate(M2, glm::vec3(-1.0f, 0.0f, 0.0f));
 
     glm::mat4 V = glm::lookAt(
         glm::vec3(0.0f, 0.0f, -5.0f),
         glm::vec3(0.0f, 0.0f, 0.0f),
         glm::vec3(0.0f, 1.0f, 0.0f));
 
-    glm::mat4 P = glm::perspective(
-        glm::radians(50.0f), 1.0f, 1.0f, 50.0f);
+    glm::mat4 P = glm::perspective(glm::radians(50.0f), 1.0f, 1.0f, 50.0f);
 
-    spLambert->use();
-    glUniform4f(spConstant->u("color"), 1, 0, 0, 1);
+    //glUniform4f(spConstant->u("color"), 1, 0, 0, 1);
     glUniform4f(spLambert->u("color"), 0, 1, 0, 1);
     glUniformMatrix4fv(spLambert->u("P"), 1, false, glm::value_ptr(P));
     glUniformMatrix4fv(spLambert->u("V"), 1, false, glm::value_ptr(V));
-    glUniformMatrix4fv(spLambert->u("M"), 1, false, glm::value_ptr(M));
+    glUniformMatrix4fv(spLambert->u("M"), 1, false, glm::value_ptr(M1));
+    Models::torus.drawSolid();
 
+    glUniform4f(spLambert->u("color"), 1, 0, 0, 1);
+    glUniformMatrix4fv(spLambert->u("P"), 1, false, glm::value_ptr(P));
+    glUniformMatrix4fv(spLambert->u("V"), 1, false, glm::value_ptr(V));
+    glUniformMatrix4fv(spLambert->u("M"), 1, false, glm::value_ptr(M2));
     Models::torus.drawSolid();
 
     glfwSwapBuffers(window);
 }
 
-
 int main(void)
 {
 	GLFWwindow* window; //Wskaźnik na obiekt reprezentujący okno
-
-	glfwSetErrorCallback(error_callback);//Zarejestruj procedurę obsługi błędów
+    
+    glfwSetErrorCallback(error_callback);//Zarejestruj procedurę obsługi błędów
 
 	if (!glfwInit()) { //Zainicjuj bibliotekę GLFW
 		fprintf(stderr, "Nie można zainicjować GLFW.\n");
@@ -113,11 +149,10 @@ int main(void)
 	initOpenGLProgram(window); //Operacje inicjujące
     float angle = 0;
     glfwSetTime(0);
-
 	//Główna pętla
 	while (!glfwWindowShouldClose(window)) //Tak długo jak okno nie powinno zostać zamknięte
 	{
-        angle += 1 * glfwGetTime();
+        angle += speed * glfwGetTime();
         
 		drawScene(window, angle); //Wykonaj procedurę rysującą
         glfwSetTime(0);
