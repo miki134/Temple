8190aafc493ea477891ba109357bc8cb19c40553
miki134 <miki.n.2001@gmail.com>
2023-05-25 17:34:57 +0200

    added second and third light

--- f_simplest.glsl
+++ f_simplest.glsl
@@ -1,32 +1,41 @@
 #version 330
 
-uniform sampler2D tex; 
+uniform sampler2D tex;
 uniform sampler2D textureMap1;
 
-out vec4 pixelColor; //Zmienna wyjsciowa fragment shadera. Zapisuje sie do niej ostateczny (prawie) kolor piksela
+out vec4 pixelColor;
 
 in vec2 iTexCoord;
 in vec2 iTexCoord1;
-in vec4 ic; 
+in vec4 ic;
 in vec4 n;
-in vec4 l;
+in vec4 l1;
+in vec4 l2;
+in vec4 l3;
 in vec4 v;
 
 void main(void) {
+    vec4 kd = mix(texture(tex, iTexCoord), texture(textureMap1, iTexCoord1), 0.3);
+    vec4 ks = vec4(1, 1, 1, 1);
 
-	//Znormalizowane interpolowane wektory
-	vec4 ml = normalize(l);
-	vec4 mn = normalize(n);
-	vec4 mv = normalize(v);
-	//Wektor odbity
-	vec4 mr = reflect(-ml, mn);
+    vec4 mn = normalize(n);
+    vec4 mv = normalize(v);
 
-	//Parametry powierzchni
-	vec4 kd = mix(texture(tex, iTexCoord), texture(textureMap1, iTexCoord1),0.3);
-	vec4 ks = vec4(1, 1, 1, 1);
+    vec4 ml1 = normalize(l1);
+    vec4 ml2 = normalize(l2);
+    vec4 ml3 = normalize(l3);
 
-	//Obliczenie modelu oświetlenia
-	float nl = clamp(dot(mn, ml), 0, 1);
-	float rv = pow(clamp(dot(mr, mv), 0, 1), 50);
-	pixelColor= vec4(kd.rgb * nl, kd.a) + vec4(ks.rgb*rv, 0);
+    vec4 mr1 = reflect(-ml1, mn);
+    vec4 mr2 = reflect(-ml2, mn);
+    vec4 mr3 = reflect(-ml3, mn);
+
+    float nl1 = clamp(dot(mn, ml1), 0, 1);
+    float nl2 = clamp(dot(mn, ml2), 0, 1);
+    float nl3 = clamp(dot(mn, ml3), 0, 1);
+
+    float rv1 = pow(clamp(dot(mr1, mv), 0, 1), 50);
+    float rv2 = pow(clamp(dot(mr2, mv), 0, 1), 50);
+    float rv3 = pow(clamp(dot(mr3, mv), 0, 1), 50);
+
+    pixelColor = vec4(kd.rgb * (nl1 + nl2 + nl3), kd.a) + vec4(ks.rgb * (rv1 + rv2 + rv3), 0);
 }
--- v_simplest.glsl
+++ v_simplest.glsl
@@ -1,34 +1,38 @@
 #version 330
 
-//Zmienne jednorodne
 uniform mat4 P;
 uniform mat4 V;
 uniform mat4 M;
 
-//Atrybuty
-in vec4 vertex; //wspolrzedne wierzcholka w przestrzeni modelu
-in vec4 color; //kolor związany z wierzchołkiem
-in vec4 normal; //wektor normalny w przestrzeni modelu
+in vec4 vertex;
+in vec4 color;
+in vec4 normal;
 in vec2 texCoord;
 
-//Zmienne interpolowane
 out vec4 ic;
-out vec4 l;
+out vec4 l1;
+out vec4 l2;
+out vec4 l3;
 out vec4 n;
 out vec4 v;
-out vec2 iTexCoord; 
+out vec2 iTexCoord;
 out vec2 iTexCoord1;
 
 void main(void) {
-    vec4 lp = vec4(0, 20, -6, 1); //pozcyja światła, przestrzeń świata
-    l = normalize(V * lp - V*M*vertex); //wektor do światła w przestrzeni oka
-    v = normalize(vec4(0, 0, 0, 1) - V * M * vertex); //wektor do obserwatora w przestrzeni oka
-    n = normalize(V * M * normal); //wektor normalny w przestrzeni oka
-    
+    vec4 lp1 = vec4(0, 20, -6, 1); // Pozycja pierwszego światła, przestrzeń świata
+    vec4 lp2 = vec4(-10, 0, 5, 1); // Pozycja drugiego światła, przestrzeń świata
+    vec4 lp3 = vec4(5, -5, 15, 1); // Pozycja trzeciego światła, przestrzeń świata
+
+    l1 = normalize(V * lp1 - V * M * vertex); // Wektor do pierwszego światła w przestrzeni oka
+    l2 = normalize(V * lp2 - V * M * vertex); // Wektor do drugiego światła w przestrzeni oka
+    l3 = normalize(V * lp3 - V * M * vertex); // Wektor do trzeciego światła w przestrzeni oka
+    v = normalize(vec4(0, 0, 0, 1) - V * M * vertex); // Wektor do obserwatora w przestrzeni oka
+    n = normalize(V * M * normal); // Wektor normalny w przestrzeni oka
+
     iTexCoord = texCoord;
     iTexCoord1 = (n.xy + 1) / 2;
 
     ic = color;
-    
-    gl_Position=P*V*M*vertex;
+
+    gl_Position = P * V * M * vertex;
 }
